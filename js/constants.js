d27ff89085eabecae5407afa25be3ea4bcf46cd8
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-01-18 08:44:58 +0100

    structurte main

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+export const PADDLE_SPEED = 8;
+export const BALL_SPEED_X = 4;
+export const BALL_SPEED_Y = -4;
+export const BRICK_ROW_COUNT = 4;
+export const BRICK_WIDTH = 100;
+export const BRICK_HEIGHT = 40;
+export const BRICK_PADDING = 10;
+export const BRICK_OFFSET_TOP = 40;
