1db89811e0fe3d0130bf1844b23936f9b928fc45
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-01-21 06:33:38 +0100

    update

--- js/ui.js
+++ js/ui.js
@@ -5,6 +5,10 @@ const livesDisplay = document.getElementById('lives');
 const scoreDisplay = document.getElementById('score');
 const startMessage = document.getElementById('start-message');
 const leveldisplay = document.getElementById('level')
+const modal = document.getElementById('game-modal')
+const modalTitle = document.getElementById('modal-title')
+const modalText = document.getElementById('modal-text');
+const modalBtn = document.getElementById('modal-btn');
 
 export function updateTimer() {
     if (state.gameRunning && !state.isPaused) {
@@ -12,7 +16,7 @@ export function updateTimer() {
     }
     let minutes = Math.floor(state.timerSecond / 60);
     let seconds = state.timerSecond % 60;
-     let formattedMin = minutes < 10 ? `0${minutes}` : minutes;
+    let formattedMin = minutes < 10 ? `0${minutes}` : minutes;
     let formattedSec = seconds < 10 ? `0${seconds}` : seconds;
     timerDisplay.innerText = `${formattedMin}:${formattedSec}`;
 }
@@ -34,6 +38,22 @@ export function showMessage(text) {
 export function hideMessage() {
     startMessage.style.display = 'none';
 }
-export function updatelevel(){
-leveldisplay.innerText=`level: ${state.currentLevel}`
+export function updatelevel() {
+    leveldisplay.innerText = `level: ${state.currentLevel}`
+}
+
+export function showModal(title, message, btnText, actionCallback) {
+    modalText.innerText = message
+    modalTitle.innerText = title
+    modalBtn.innerText = btnText
+
+    modal.style.display = 'flex'
+    modalBtn.onclick = function () {
+        modal.style.display = 'none'
+        if (actionCallback) {
+            actionCallback()
+        } else {
+            document.location.reload
+        }
+    }
 }
